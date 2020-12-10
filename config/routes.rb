691bec452d4129e4b4554d898c28849ce361b912
Jakob Persons <jakobep24@gmail.com>
2020-12-10 10:29:26 -0500

    built route with params

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  # '/:id' tells routing system this route can receive parameter
+  # parameter will be passed ot the controller's show action
+  resources :posts, only: :show
+
+
 end
